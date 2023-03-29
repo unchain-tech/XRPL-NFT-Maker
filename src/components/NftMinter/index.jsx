6dab4575958f6f00fdcd802efdf6b5ec4efd870c
tequ <git@tequ.dev>
2023-03-29 20:40:34 +0900

    starter

---
+++ src/components/NftMinter/index.jsx
@@ -0,0 +1,23 @@
+import { Button } from "@mui/material";
+import { Xumm } from "xumm";
+import "./index.css";
+
+const xumm = new Xumm('api-key')
+
+export const NftMinter = () => {
+  return (
+    <div className="nft-minter-box">
+      <div className="title">XRP NFT</div>
+      <div className="image-box">
+        <Button variant="contained">
+          ファイルを選択
+          <input
+            className="imageInput"
+            type="file"
+            accept=".jpg , .jpeg , .png"
+          />
+        </Button>
+      </div>
+    </div>
+  );
+}
